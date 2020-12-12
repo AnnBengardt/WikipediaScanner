376d98ce05f053f632b942ec155554db90b2a553
AnnBengardt <marunko.a@yandex.ru>
2020-12-12 20:03:02 +0300

    added comments

--- src/main/java/wikipediaScanner/Main.java
+++ src/main/java/wikipediaScanner/Main.java
@@ -10,9 +10,13 @@ public class Main {
         while (!stop) {
             final Scanner scanner = new Scanner(System.in);
             System.out.println("\nSearch for...");
+
+            // ожидается поисковой запрос на английском (!!)
             String nextLine = scanner.nextLine();
             if (nextLine != null) {
-                System.out.println("Searching on the web....");
+                System.out.println("Searching on the web...");
+
+                // запускаем поиск полученного запроса по википедии
                 WikipediaScanner wikipediaScanner = new WikipediaScanner(nextLine);
             }
         }
--- src/main/java/wikipediaScanner/WikipediaScanner.java
+++ src/main/java/wikipediaScanner/WikipediaScanner.java
@@ -12,8 +12,11 @@ import java.util.stream.Collectors;
 
 
 public class WikipediaScanner {
+    // переменные для запроса пользователя и ответа API Википедии
     String request;
     String wikipediaJson = "";
+
+    // Ссылки для получения статей с API
     String apiURL = "https://www.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=";
     String randomURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&generator=random&grnnamespace=0";
 
@@ -26,32 +29,52 @@ public class WikipediaScanner {
         final String encoding = "UTF-8";
         Document search;
 
+        /*
+        Здесь проверка корректности запроса пользователя и добавление к нему слова "wikipedia"
+        Мне показалось, что находить статью в поисковике и парсить её название по первой ссылке результата поиска
+        будет более гибким решением, чем просить у пользователя точное название статьи. Таким образом, можно вбивать
+        в программу более "очеловеченные" запросы. Например, "что такое солнце"? Или "кто такой Иван Грозный"?
+         */
         assert request != null;
         request = request + " wikipedia";
 
         try {
+            // запрос к гуглу
             search = Jsoup.connect("https://www.google.com/search?q="
                     + URLEncoder.encode(request, encoding)).userAgent("Chrome/87.0.4280.88").get();
 
+            // на обычной странице с результатами поиска первая ссылка находится в шестнадцатом тэге <a></a>
             String wikipediaURL = search.getElementsByTag("a").get(16).text();
 
-            if (wikipediaURL.indexOf("-") != -1){
-                wikipediaJson = apiURL + wikipediaURL.substring(0, wikipediaURL.indexOf("-")-1).replaceAll(" ", "_");
+            /*
+            с помощью инструментов строк берём из тэга заголовк статьи, если же нашлось что-то неверное или ничего не
+            нашлось, то программа предложит пользователю рандомную статью, полученную тоже с помощью API.
+             */
+            if (wikipediaURL.contains("wikipedia.org")){
+                wikipediaJson = apiURL + wikipediaURL.substring(0, wikipediaURL.indexOf("- ")-1).replaceAll(" ", "_");
             } else {
                 System.out.println("Nothing found but read this:");
                 wikipediaJson = randomURL;
             }
 
+            // GET-запрос по ссылке для API, добавляем устройство
             HttpURLConnection http = (HttpURLConnection) new URL(wikipediaJson).openConnection();
             http.addRequestProperty("User-Agent", "Chrome/87.0.4280.88");
+            // получаем содержимое страницы и прочитываем его с помощью BufferedReader (всё читается как стрим)
             BufferedReader inquiry = new BufferedReader(new InputStreamReader(http.getInputStream()));
 
+            // читаем все полученные строки и записываем в переменную
             String response = inquiry.lines().collect(Collectors.joining());
             inquiry.close();
 
+            // так как нужна краткая версия статьи, из json-ответа нужен лишь extract
             String result = response.split("extract\":\"")[1];
+
+            // обычное форматирование для лучшего восприятия пользователем ответа и его вывод
             result = result.replaceAll("}", "");
             System.out.println(result.replaceAll("(.{125})", "$1\n"));
+
+            // добавление ссылки на полную статью
             wikipediaURL = "https://en.wikipedia.org/wiki/"
                     +response.split("title\":\"")[1].split("\",")[0].replaceAll(" ", "_");
             System.out.println("Read more here:");
